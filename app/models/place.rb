a8af28f18bb855541fafcb22dbf717dbcca1b09c
Farrah Lee <farrahyglee@yahoo.com>
2015-10-02 00:56:45 +0000

    Update

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,10 @@
 class Place < ActiveRecord::Base
+	belongs_to :user
+	geocoded_by :address
+	after_validation :geocode
+
+	validates :name, length: {minimum: 3}, :presence => true
+	validates :address, :presence => true
+	validates :description, :presence => true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001064152) do
+ActiveRecord::Schema.define(version: 20151001184407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,12 @@ ActiveRecord::Schema.define(version: 20151001064152) do
     t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.float    "latitude"
+    t.float    "longitude"
   end
+
+  add_index "places", ["user_id"], name: "index_places_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
